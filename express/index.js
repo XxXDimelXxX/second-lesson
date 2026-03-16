1904340e9377c12d2f7684556ba0b6eeae298f47
XXXDimMelXXX <melnikoffdd13@gmail.com>
2026-03-16 11:45:12 +0300

    first-commit

---
+++ express/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json());
+
+let users = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'Alex'},
+];
+
+app.get('/users', (req, res) => res.json(users));
+
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name
+    }
+
+    users.push(newUser);
+
+    res.json(newUser);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    users = users.filter(user => user.id !== id);
+
+    res.json({message: 'User deleted'});
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
